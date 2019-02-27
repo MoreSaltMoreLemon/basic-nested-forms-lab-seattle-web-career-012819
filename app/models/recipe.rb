de7611c4246eb29d92a786a6b71a1b9dbca62a3f
github username <github email address>
2019-02-27 11:56:03 -0800

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  # def ingredients_attributes(ingredients)
+  #   ingredients.each do |ingredient|
+  #     self.ingredients.build(ingredient)
+  #   end
+  # end
+
 end
--- spec/features/recipe_form_spec.rb
+++ spec/features/recipe_form_spec.rb
@@ -4,6 +4,7 @@ require 'capybara/rspec'
 describe "the recipe form", :type => :feature do
   it "sets the title" do
     visit '/recipes/new'
+    page.save_page
     fill_in :recipe_title, with: 'Chocolate Cake'
     find('input[name="commit"]').click
     expect(Recipe.last.title).to eq 'Chocolate Cake'
